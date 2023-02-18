0d4b590c5da8786ca9ad3586855bb57368341c63
Thangallapally <sai-teja.thangallapally@capgemini.com>
2023-02-18 20:20:20 +0530

    first commit

--- Employees/urls.py
+++ Employees/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import employee_list,employee_detail,employee_delete,home,employee_edit,employee_add,team_list,team_detail,team_edit,employee_note,login_view,signup_view,logout_view,organization,team_add
+from .views import employee_list,employee_detail,employee_delete,home,employee_edit,employee_add,team_list,team_detail,team_delete,team_edit,employee_note,login_view,signup_view,logout_view,organization,team_add
 
 urlpatterns = [
     path("",home,name='home'),
@@ -10,6 +10,7 @@ urlpatterns = [
     path("Teams",team_list,name="team_list"),
     path("Teams/<int:id>",team_detail,name="team_detail"),
     path("Team_edit/<int:id>",team_edit,name="team_edit"),
+    path("Team_delete/<int:id>",team_delete,name="team_delete"),
     path("Employee_notes/<int:id>",employee_note,name="employee_note"),
     path("login",login_view,name="login"),
     path("signup",signup_view,name="signup"),
